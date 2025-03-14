f013cb222a9e024937e3806825ac77452ad5f761
Beast <mjnuvor@gmail.com>
2025-03-14 00:08:10 +0000

    tested new registration

--- Backend/src/models/usertest.js
+++ Backend/src/models/usertest.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const { registerUser } = require('./User.js')
+
 
 async function testRegister() {
     try {
-        const result = await registerUser("Joe", "mj@gmail.com", "sfbsdiuf");
+        const result = await registerUser("Joe", "m@gmail.com", "sfbsdiuf");
         console.log(result)
     } catch (error) {
-        throw new Error(error);
-
+        console.error("Error in user registration", error.message)
     }
 }
 
